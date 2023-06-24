869c24c746486d99fa693f085726ce8a191ef873
Nandini <juicyfruit5600@yahoo.ca>
2023-06-24 03:21:31 -0400

    Updated PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public CharacterController2D controller2D;
+    float horizMove = 0f;
+    public float speed = 40f;
+    bool jump = false;
     void Start()
     {
         
@@ -14,6 +17,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizMove = Input.GetAxisRaw("Horizontal") * speed;
+        //
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller2D.Move(horizMove * Time.fixedDeltaTime ,false, jump);
+        jump = false;
     }
 }
